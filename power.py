7aa4d2a1583ac39dcb944b1b10a512cd701de243
subhas manna <subhasmanna875@gmail.com>
2023-11-15 18:48:37 +0530

    using while else construction

--- power.py
+++ power.py
@@ -1,15 +1,13 @@
+# using while else  construction
 L = [1, 2, 4, 8, 16, 32, 64]
 X = 5
-found = False
 i = 0
 
-while not found and i < len(L):
+while i < len(L):
     if 2 ** X == L[i]:
-        found = True
-    else:
-        i = i + 1
-
-if found:
-    print('at index', i)
+        print('at index', i)
+        break
+    i = i + 1
 else:
     print(X, 'not found')
+

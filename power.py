8bf6c7147688a06aab6123c0f746f7145255f1e8
subhas manna <subhasmanna875@gmail.com>
2023-11-15 23:39:26 +0530

    Use a for loop and list append method to generate the powers-of-2 list

--- power.py
+++ power.py
@@ -1,6 +1,9 @@
-#Remove the loop using the in operator membership expression
-L = [2 ** i for i in range(7)]
+#Use a for loop and list append method to generate the powers-of-2 list
+L = []
 X = 5
+
+for i in range(7):
+    L.append(2 ** i)
 
 if 2 ** X in L:
     print('at index', L.index(2 ** X))

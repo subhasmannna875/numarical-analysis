5011b1acff1063cbe3193dbd64a37b717f90d5be
subhas manna <subhasmanna875@gmail.com>
2023-11-15 23:35:30 +0530

    remove the loop using the in operator membership experession

--- power.py
+++ power.py
@@ -1,11 +1,8 @@
-# using for- else  construction
-L = [1, 2, 4, 8, 16, 32, 64]
+#Remove the loop using the in operator membership expression
+L = [2 ** i for i in range(7)]
 X = 5
 
-try:
-    index = L.index(2 ** X)
-    print('at index', index)
-except ValueError:
+if 2 ** X in L:
+    print('at index', L.index(2 ** X))
+else:
     print(X, 'not found')
-
-
